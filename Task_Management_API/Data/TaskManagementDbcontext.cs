07894b6402f0a757d159b09100b7a9283e3739be
72591_Kedar <kedar.jagtap999@gmail.com>
2025-05-18 11:21:27 +0530

    adding dbcontext class and dbset properties

---
+++ Task_Management_API/Data/TaskManagementDbcontext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Task_Management_API.Data
+{
+    public class TaskManagementDbcontext : DbContext
+    {
+        public TaskManagementDbcontext(DbContextOptions dbContextOptions) : base(dbContextOptions) 
+        {
+            
+        }
+
+        // DbSet of an task entity
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
